5475999e6f960a7100956c68bf035978a2828cb4
Curtis Lee <curtis@unieye.co.kr>
2017-12-28 09:44:50 +0900

    opencv 경로 이상 문제 수정

--- CopyCommon_x64.bat
+++ CopyCommon_x64.bat
@@ -10,7 +10,8 @@ copy ThirdParty\Data\XComProW_M40_01_64.dll DependenctDll
 copy ThirdParty\ImagingLibrary\EmguCV2.4.9_x64\Emgu.*.dll ReferenceDll
 copy ThirdParty\ImagingLibrary\EmguCV2.4.9_x64\opencv_.*.dll DependenctDll
 copy ThirdParty\ImagingLibrary\EmguCV2.4.9_x64\cu*.dll DependenctDll
-copy ThirdParty\ImagingLibrary\EmguCV2.4.9_x64\cv*.dll DependenctDll
+copy ThirdParty\ImagingLibrary\EmguCV2.4.9_x64\opencv*.dll DependenctDll
+copy ThirdParty\ImagingLibrary\EmguCV2.4.9_x64\opencv*.zip DependenctDll
 copy ThirdParty\ImagingLibrary\EmguCV2.4.9_x64\npp*.dll DependenctDll
 copy ThirdParty\ImagingLibrary\EmguCV2.4.9_x64\OpenCL.dll DependenctDll
 copy ThirdParty\ImagingLibrary\Matrox.MatroxImagingLibrary.dll ReferenceDll
