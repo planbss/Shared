c0add8c40247257f64d8667cc9797af1c1b67e94
MinhyeongCho <fly_mh@naver.com>
2019-07-17 18:11:04 +0900

    AutoFocus library 추가

--- CopyCommon_x64.bat
+++ CopyCommon_x64.bat
@@ -40,6 +40,7 @@ copy ThirdParty\DeviceEtc\HASP\hasp_net_windows.dll ReferenceDll
 copy ThirdParty\DeviceEtc\HASP\apidsp_windows_x64.dll DependenctDll
 copy ThirdParty\DeviceEtc\MxComponent\AxInterop.ActUtlTypeLib.dll ReferenceDll
 copy ThirdParty\DeviceEtc\MxComponent\Interop.ActUtlTypeLib.dll DependenctDll
+copy ThirdParty\DeviceEtcOptoTune\*.dll ReferenceDll
 
 7z x DependenctDll\*.7z -aoa -oDependenctDll\
 7z x DependenctDll\*.7z -aoa -oDependenctDll\
