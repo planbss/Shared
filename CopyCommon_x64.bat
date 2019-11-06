1b94477bc2489d3ee7757b6b74ed1108bf7e2bfc
vassago89 <vassago89@gmail.com>
2019-11-06 15:41:13 +0900

    배치 파일에 Optotune Dll 복사 되도록 수정

--- CopyCommon_x64.bat
+++ CopyCommon_x64.bat
@@ -39,7 +39,7 @@ copy ThirdParty\DeviceEtc\HASP\hasp_net_windows.dll ReferenceDll
 copy ThirdParty\DeviceEtc\HASP\apidsp_windows_x64.dll DependenctDll
 copy ThirdParty\DeviceEtc\MxComponent\AxInterop.ActUtlTypeLib.dll ReferenceDll
 copy ThirdParty\DeviceEtc\MxComponent\Interop.ActUtlTypeLib.dll DependenctDll
-copy ThirdParty\DeviceEtcOptoTune\*.dll ReferenceDll
+copy ThirdParty\DeviceEtc\OptoTune\*.dll ReferenceDll
 
 7z x DependenctDll\*.7z -aoa -oDependenctDll\
 7z x DependenctDll\*.7z -aoa -oDependenctDll\
