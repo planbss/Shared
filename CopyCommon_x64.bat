0311f5f3e3209bb89e16cced7d44a669c948aa45
조준형 <scvcs1@unieye.co.kr>
2021-12-15 16:21:14 +0900

    OpenVino 배치사이즈 적용된 버전으로 변경

--- CopyCommon_x64.bat
+++ CopyCommon_x64.bat
@@ -78,6 +78,7 @@ copy ThirdParty\RsAutomationX8\x64\bin\*.dll ReferenceDll
 
 copy ThirdParty\MachineLearning\CNTK\Cntk.Core.Managed-*.dll ReferenceDll
 copy ThirdParty\MachineLearning\OpenVino\Release\bin\*.dll DependencyDll
+copy ThirdParty\MachineLearning\OpenVino\Release\bin\*.xml DependencyDll
 copy ThirdParty\MachineLearning\OpenVino\Release\lib\*.dll ReferenceDll
 
 copy ThirdParty\MachineLearning\ICP\ICP.dll ReferenceDll
